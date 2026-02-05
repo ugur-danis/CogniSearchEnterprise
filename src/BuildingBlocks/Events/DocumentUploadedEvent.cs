5aa0201b6ff82d64c9564722d76cd855a5148d20
ugurdanis <ugur.danis@detaysoft.com>
2026-02-05 09:48:12 +0300

    refactor(BuildingBlocks): Add DocumentUploadedEvent and remove unused Class1
    - Add DocumentUploadedEvent record for document upload messaging
    - Remove boilerplate Class1.cs

---
+++ src/BuildingBlocks/Events/DocumentUploadedEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace BuildingBlocks.Events;
+
+public record DocumentUploadedEvent
+{
+    public Guid Id { get; init; }
+    public Guid UserId { get; init; }
+    public string Title { get; init; } = default!;
+    public string FilePath { get; init; } = default!;
+}
